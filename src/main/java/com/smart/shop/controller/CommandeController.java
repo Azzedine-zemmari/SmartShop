5309e5ee30860355e84ae8912f4fbcba930fded3
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-11-26 13:30:00 +0100

    Feature :  ajouter endpoint pour cree une commande

---
+++ src/main/java/com/smart/shop/controller/CommandeController.java
@@ -0,0 +1,24 @@
+package com.smart.shop.controller;
+
+import com.smart.shop.dto.CommandeRequestDto;
+import com.smart.shop.service.commande.CommandeService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/commande")
+@AllArgsConstructor
+public class CommandeController {
+    private CommandeService commandeService;
+
+    @PostMapping("/create")
+    public ResponseEntity<CommandeRequestDto> createCommande(@RequestBody CommandeRequestDto dto ){
+        CommandeRequestDto commandeRequestDto = commandeService.createCommande(dto);
+        return ResponseEntity.ok(commandeRequestDto);
+    }
+}
