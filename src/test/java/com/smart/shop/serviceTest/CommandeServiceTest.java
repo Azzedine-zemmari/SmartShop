35e809ad240a799d360ea4c27729c9c073e254a8
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-11-28 12:30:00 +0100

    Test : test method create commande

--- src/test/java/com/smart/shop/serviceTest/CommandeServiceTest.java
+++ src/test/java/com/smart/shop/serviceTest/CommandeServiceTest.java
@@ -2,10 +2,15 @@ package com.smart.shop.serviceTest;
 
 import com.smart.shop.dto.CommandeRequestDto;
 import com.smart.shop.dto.OrderItemRequestDto;
+import com.smart.shop.enums.Niveau_fidelete;
+import com.smart.shop.enums.OrderStatus;
 import com.smart.shop.exeception.ProductNotFoundException;
 import com.smart.shop.exeception.UserNotFound;
 import com.smart.shop.mapper.CommandeMapper;
 import com.smart.shop.model.Client;
+import com.smart.shop.model.Commande;
+import com.smart.shop.model.OrderItem;
+import com.smart.shop.model.Product;
 import com.smart.shop.repository.ClientRepository;
 import com.smart.shop.repository.CommandeRepository;
 import com.smart.shop.repository.OrderItemsRepository;
@@ -69,5 +74,89 @@ public class CommandeServiceTest {
 
         assertThrows(ProductNotFoundException.class,()-> commandeService.createCommande(dto));
 
+    }
+    @Test
+    void shouldCreateRejectedCommande_whenStockInsufficient() {
+        CommandeRequestDto dto = new CommandeRequestDto();
+        dto.setClientId(1);
+
+        OrderItemRequestDto item = new OrderItemRequestDto();
+        item.setProductId(5);
+        item.setQuantity(10);
+
+        dto.setItems(List.of(item));
+
+        Client client = new Client();
+        client.setId(1);
+
+        Product product = new Product();
+        product.setId(5);
+        product.setStock_disponible(3);
+        product.setPrix_unitaire(100.0);
+
+
+        Commande commandeMapped = new Commande();
+        commandeMapped.setOrderItems(List.of(new OrderItem()));
+
+        Mockito.when(commandeMapper.toEntity(dto)).thenReturn(commandeMapped);
+        Mockito.when(clientRepository.findById(1)).thenReturn(Optional.of(client));
+        Mockito.when(productRepository.findById(5)).thenReturn(Optional.of(product));
+
+        Mockito.when(commandeRepository.save(Mockito.any())).thenAnswer(inv -> inv.getArgument(0));
+
+        CommandeRequestDto result = commandeService.createCommande(dto);
+
+        Mockito.verify(commandeRepository).save(Mockito.any());
+
+        Mockito.verify(commandeMapper).toRequestDto(
+                Mockito.argThat(cmd ->
+                        cmd.getStatus() == OrderStatus.REJECTED &&
+                                cmd.getTotal() == 0
+                )
+        );
+
+    }
+
+    @Test
+    void shouldCreateSuccessCommande(){
+        CommandeRequestDto dto = new CommandeRequestDto();
+        dto.setClientId(1);
+
+        OrderItemRequestDto item = new OrderItemRequestDto();
+        item.setProductId(1);
+        item.setQuantity(5);
+
+        dto.setItems(List.of(item));
+
+        Product product = new Product();
+        product.setId(1);
+        product.setNom("jean");
+        product.setStock_disponible(40);
+        product.setPrix_unitaire(50.22);
+
+        Client client = new Client();
+        client.setId(1);
+        client.setNiveau_fidelete(Niveau_fidelete.BASIC);
+
+        Commande commandeMapped = new Commande();
+        commandeMapped.setOrderItems(List.of(new OrderItem()));
+
+        Mockito.when(commandeMapper.toEntity(dto)).thenReturn(commandeMapped);
+        Mockito.when(clientRepository.findById(1)).thenReturn(Optional.of(client));
+        Mockito.when(productRepository.findById(1)).thenReturn(Optional.of(product));
+
+        Mockito.when(commandeRepository.save(Mockito.any())).thenAnswer(inv -> inv.getArgument(0));
+
+        Mockito.when(commandeMapper.toRequestDto(Mockito.any()))
+                .thenReturn(new CommandeRequestDto());
+
+        Mockito.when(commandeRepository.countByClientIdAndStatus(1, OrderStatus.CONFIRMED))
+                .thenReturn(5);
+        Mockito.when(commandeRepository.sumTotalByClientId(1, OrderStatus.CONFIRMED))
+                .thenReturn(2000.0);
+
+        CommandeRequestDto result = commandeService.createCommande(dto);
+
+        Mockito.verify(commandeRepository).save(Mockito.any());
     }
 }
